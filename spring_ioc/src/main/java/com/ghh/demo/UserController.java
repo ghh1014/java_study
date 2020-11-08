c093591b6ea672c621e34a24a2ddcd54988bc291
郭华华 <guohuahua1014@163.com>
2020-11-08 20:54:03 +0800

    细微调整

--- spring_ioc/src/main/java/com/ghh/demo/UserController.java
+++ spring_ioc/src/main/java/com/ghh/demo/UserController.java
@@ -5,10 +5,12 @@ import com.ghh.service.IUserService;
 import com.ghh.service.impl.UserServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class UserController {
     public static void main(String[] args) {
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext app = new FileSystemXmlApplicationContext("E:\\java_study\\spring_ioc\\src\\main\\resources\\applicationContext.xml");
         IUserService userService = app.getBean("userService", IUserService.class);
         //IUserService userService = new UserServiceImpl();
         userService.save();
