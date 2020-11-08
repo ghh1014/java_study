1cb3e03cf9f81a13175aa819096bce5cdefb048f
郭华华 <guohuahua1014@163.com>
2020-11-08 22:24:04 +0800

    使用ResourceBundle读取数据源配置文件

--- spring/spring_ioc_anno/src/test/java/com/ghh/test/DataSourceTest.java
+++ spring/spring_ioc_anno/src/test/java/com/ghh/test/DataSourceTest.java
@@ -4,7 +4,27 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
 import org.junit.Test;
 
+import java.util.ResourceBundle;
+
 public class DataSourceTest {
+
+    /**
+     * 测试手动创建数据源(加载properties配置文件)
+     */
+    @Test
+    public void test3() throws Exception{
+        //读取配置文件
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        //创建数据源
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(rb.getString("jdbc.driver"));
+        dataSource.setUrl(rb.getString("jdbc.url"));
+        dataSource.setUsername(rb.getString("jdbc.username"));
+        dataSource.setPassword(rb.getString("jdbc.password"));
+        DruidPooledConnection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
 
     /**
      * 测试手动创建数据源
