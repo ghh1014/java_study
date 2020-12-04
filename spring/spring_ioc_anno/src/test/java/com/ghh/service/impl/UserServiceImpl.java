3e4df7d1b56ccd74926f5e479d2587bc054d414e
郭华华 <guohuahua1014@163.com>
2020-12-04 14:18:27 +0800

    spring集成JUnit

--- spring/spring_ioc_anno/src/test/java/com/ghh/service/impl/UserServiceImpl.java
+++ spring/spring_ioc_anno/src/test/java/com/ghh/service/impl/UserServiceImpl.java
@@ -2,19 +2,26 @@ package com.ghh.service.impl;
 
 import com.ghh.dao.IUserDao;
 import com.ghh.service.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
+//@Scope("prototype")
 public class UserServiceImpl implements IUserService {
 
-    @Autowired
-    @Qualifier("userDao")
+    @Value("${jdbc.driver}")
+    private String driver;
+
+    //@Autowired
+    //@Qualifier("userDao") //Qualifier可指定Id,但是它需要和Autowired一起出现
+    @Resource(name = "userDao")
     private IUserDao userDao;
 
     @Override
     public void save() {
+        System.out.println(driver);
         userDao.save();
     }
 }
--- spring/spring_ioc_anno/src/test/java/com/ghh/web/UserController.java
+++ spring/spring_ioc_anno/src/test/java/com/ghh/web/UserController.java
@@ -1,12 +1,14 @@
 package com.ghh.web;
 
+import com.ghh.config.SpringConfiguration;
 import com.ghh.service.IUserService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserController {
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext app = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         IUserService userService = app.getBean(IUserService.class);
         userService.save();
     }
