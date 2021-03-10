aa1ff61298ccd38db26a2f5fb7ed4417b003f924
郭华华 <guohuahua1014@163.com>
2021-03-10 23:43:58 +0800

    spring-mvc快速入门

--- spring/spring_mvc/src/main/java/com/ghh/controller/UserController.java
+++ spring/spring_mvc/src/main/java/com/ghh/controller/UserController.java
@@ -2,13 +2,15 @@ package com.ghh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/quick")
+    @RequestMapping(value = "/quick",method = RequestMethod.GET,params = {"username"})
     public String save(){
         System.out.println("User Controller save running.....");
-        return "success.jsp";
+        return "success";
     }
 }
