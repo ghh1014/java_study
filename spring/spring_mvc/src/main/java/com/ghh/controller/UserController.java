c745467845e50b3154ac8f37164b2a89a9822653
郭华华 <guohuahua1014@163.com>
2021-03-17 14:54:55 +0800

    mvc控制器返回json格式数据

--- spring/spring_mvc/src/main/java/com/ghh/controller/UserController.java
+++ spring/spring_mvc/src/main/java/com/ghh/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.ghh.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ghh.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,22 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/quick6")
+    @ResponseBody
+    public User save6() throws Exception {
+        User user = new User("ghh",30);
+        return user;
+    }
+
+    @RequestMapping("/quick5")
+    @ResponseBody
+    public String save5() throws Exception {
+        User user = new User("ghh",30);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = objectMapper.writeValueAsString(user);
+        return jsonStr;
+    }
 
     @RequestMapping("/quick4")
     @ResponseBody
